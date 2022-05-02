8ed70a9767b9563c11dc2a6354796cab5f1981c6
Shubham Deshpande <shubham14deshpande@gmail.com>
2022-05-02 15:53:38 +0530

    made responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar(props) {
       <div className="container-fluid">
         <Link className="navbar-brand" id='name' to="/shubham_deshpande/">{props.name}</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          {/* <span className="navbar-toggler-icon"></span> */}
         </button>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
