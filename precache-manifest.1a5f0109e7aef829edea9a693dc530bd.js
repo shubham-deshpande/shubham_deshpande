f53b0b53102783547f82d23964eef11a50b84438
Shubham Deshpande <shubham14deshpande@gmail.com>
2022-05-02 09:48:06 +0530

    Updates

---
+++ precache-manifest.1a5f0109e7aef829edea9a693dc530bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01c2b587b234dfcecde9",
+    "url": "/shubham_deshpande/static/css/main.b7814aac.chunk.css"
+  },
+  {
+    "revision": "01c2b587b234dfcecde9",
+    "url": "/shubham_deshpande/static/js/main.098a4902.chunk.js"
+  },
+  {
+    "revision": "60f160f1a3985ae2e3c4",
+    "url": "/shubham_deshpande/static/js/runtime~main.9e0d30e4.js"
+  },
+  {
+    "revision": "c4ead0f5515cecb5f411",
+    "url": "/shubham_deshpande/static/js/2.a2df5f33.chunk.js"
+  },
+  {
+    "revision": "408621d3a15eaf977c3c",
+    "url": "/shubham_deshpande/static/js/3.87f4fc67.chunk.js"
+  },
+  {
+    "revision": "ac7027fcd96c0154950250ef8b1acf2c",
+    "url": "/shubham_deshpande/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d80ffb427926ee4bda187039ab9fdc63.js"
+  "/shubham_deshpande/precache-manifest.1a5f0109e7aef829edea9a693dc530bd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/shubham_deshpande/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
