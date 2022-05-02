3f0870171ae180374cde587cc21717acab89cd70
Shubham Deshpande <shubham14deshpande@gmail.com>
2022-05-02 09:20:54 +0530

    Updates

---
+++ precache-manifest.900ac84ab05f958f6e66a6ab86eefb2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d25cd20abc8c40ca1729",
+    "url": "/shubham_deshpande/static/css/main.b7814aac.chunk.css"
+  },
+  {
+    "revision": "d25cd20abc8c40ca1729",
+    "url": "/shubham_deshpande/static/js/main.b5d55f93.chunk.js"
+  },
+  {
+    "revision": "60f160f1a3985ae2e3c4",
+    "url": "/shubham_deshpande/static/js/runtime~main.9e0d30e4.js"
+  },
+  {
+    "revision": "c4ead0f5515cecb5f411",
+    "url": "/shubham_deshpande/static/js/2.a2df5f33.chunk.js"
+  },
+  {
+    "revision": "408621d3a15eaf977c3c",
+    "url": "/shubham_deshpande/static/js/3.87f4fc67.chunk.js"
+  },
+  {
+    "revision": "5520cc7b292d0aae5642b994bcafbc7c",
+    "url": "/shubham_deshpande/index.html"
+  }
+];
